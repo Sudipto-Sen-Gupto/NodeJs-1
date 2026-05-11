78bd26ccb15d38d26ed9a258c6269b45a26ef2de
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-11 17:18:18 +0600

    how to show data from database on ui by using file system

--- src/controller/products-controller.ts
+++ src/controller/products-controller.ts
@@ -1,22 +1,23 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { serviceData } from "../services/service";
 
 
  export const products=(req:IncomingMessage,res:ServerResponse)=>{
      const url=req.url;
      const method=req.method;
       
-     const products=[
-        {
-            id:1234,
-            product_name:"Laptop"
-        }
-        ,
-        {
-            id:1235,
-            product_name:"Mobile"
-        }
-     ]
-
+    //  const products=[
+    //     {
+    //         id:1234,
+    //         product_name:"Laptop"
+    //     }
+    //     ,
+    //     {
+    //         id:1235,
+    //         product_name:"Mobile"
+    //     }
+    //  ]
+     const products=  serviceData()
      if(url==='/products'&& method==='GET'){
         res.writeHead(200,{"Content-Type":"application/json"})
         res.end(JSON.stringify({message:"products retrieve",data:products}))
---
+++ src/services/service.ts
@@ -0,0 +1,15 @@
+import fs from "fs"
+import path from "node:path";
+
+ const filePath=path.join(process.cwd(),"./src/database/data.json")
+
+ export const serviceData=()=>{
+       //  console.log(process.cwd());//current work directory 
+       //  console.log(filePath); 
+       const productFromDatabase=fs.readFileSync(filePath,'utf-8') //alternate way to make buffer data to string
+       // console.log(productFromDatabase); //buffering data
+       // console.log(productFromDatabase.toString()); //stringify data
+       // console.log(productFromDatabase);
+       console.log(JSON.parse(productFromDatabase));
+       return JSON.parse(productFromDatabase)
+ }
